93e7a1bd98ecd228d0d7389b8eef701ab0100ccb
thevijaysoni <vijaysoni96670@gmail.com>
2020-08-18 12:48:25 +0530

    VIjay Soni

--- src/FirebaseClient/FirebaseClient.js
+++ src/FirebaseClient/FirebaseClient.js
@@ -1,14 +1,14 @@
 import firebase from 'react-native-firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDrEggooc3j88WYsa9lUUFD3mkY73p9w9o",
-    authDomain: "srdevops-corona-tracker.firebaseapp.com",
-    databaseURL: "https://srdevops-corona-tracker.firebaseio.com",
-    projectId: "srdevops-corona-tracker",
-    storageBucket: "srdevops-corona-tracker.appspot.com",
-    messagingSenderId: "1071012521733",
-    appId: "1:1071012521733:web:a68bedc269810a848db96c",
-    measurementId: "G-XBT3SG1LPF"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
